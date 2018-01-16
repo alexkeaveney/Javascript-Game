c62cb9553eaa44b5c901820488e4e6cb2e9cc0e3
Alex Keaveney <ak12349@hotmail.com>
2018-01-16 21:56:12 +0000

    sign in up and cookies

--- node_files/app.js
+++ node_files/app.js
@@ -1,132 +1,104 @@
-//server.js
 
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const parser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 let clientCount = 0;
 let users = [];
 let db;
 
 app.use(express.static(__dirname + '/node_modules'));
+app.use(session({secret: 'keyboard cat', resave: false, saveUninitialized: true,cookie: { path: '/', httpOnly: true, maxAge: 30 * 30000 },rolling: true}));
+//parses the url
+app.use(parser.urlencoded({ extended: true}));
+// Parses the text as JSON and exposes the resulting object on req.body.
+app.use(parser.json());
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+
+    if (req.session.user) {
+        res.sendFile(__dirname + '/public/lobby.html');
+    }
+    else {
+        res.sendFile(__dirname + '/public/index.html');
+    }
+
 });
-//
+
 app.get('/howto', function(req, res) {
-  res.sendFile(__dirname + '/public/howto.html');
+    res.sendFile(__dirname + '/public/howto.html');
 });
+
 app.get('/lobby', function(req, res) {
-  res.sendFile(__dirname + '/public/lobby.html');
+    console.log(req.session.user);
+    res.sendFile(__dirname + '/public/lobby.html');
 });
 
 const MongoClient = require('mongodb').MongoClient
 
 MongoClient.connect('mongodb://127.0.0.1:27017', (err, database) => {
-  if (err) {
-    console.log(err);
-  }
-  else {
-    db = database.db('cheat');
 
+    if (err) { console.log(err); }
+    else { db = database.db('cheat'); }
 
-
-  }
-  app.listen(8080, () => {
-    console.log('listening on 8080');
-    // this.db.collection('users').insertOne({name: 'Joe', password: "test"});
-  });
-
-
-
-
-  //db.collection.insertOne({user: {name: 'Joe', password: "test"}});
+    app.listen(8080, () => {
+        console.log('Database connected');
+    });
 });
 
-//});
 
 
-app.use(parser.urlencoded({
-    extended: true
-}));
-
-/**bodyParser.json(options)
- * Parses the text as JSON and exposes the resulting object on req.body.
- */
-app.use(parser.json());
 
 app.post('/', (req, res) => {
 
-  // db.collection('users').save(req.body, (err, result) => {
-  // if (err) return console.log(err)
-  //   console.log('saved to database')
-  //   res.redirect('/')
-  // });
+    const user = req.body.user.name;
+    const pass = req.body.user.password;
 
+    //check if the user is in the database
+    db.collection('users').findOne({name: user}, function (findErr, result) {
+        //if the user exists
+        if (result !== null) {
+            //if their password is correct
+            if (pass === result.password) {
+                //creates session for user
+                req.session.user = user;
+                req.session.save();
+                //sends the lobby file
+                res.sendFile(__dirname + '/public/lobby.html');
+            }
+            else {
+                //res.send("Login details incorrect");
+            }
+        }
+        //create a new user
+        else {
+            db.collection('users').insertOne({name: user, password: pass}, function(err, result) {
+                console.log(result);
+                //creates session for new user
+                req.session.user = user;
+                req.session.save();
+                //sends the lobby file
+                res.sendFile(__dirname + '/public/lobby.html');
+            });
+        }
+    });
 
-  //console.log(req.body.user.name);
-
-  const user = req.body.user.name;
-  const pass = req.body.user.password;
-
-  //console.log(req.body.user.password);
-
-  //check if the user is in the database
-
-  db.collection('users').findOne({}, function (findErr, result) {
-
-    //if the user exists
-    if (result !== null) {
-      if (pass === result.password) {
-          //sign him in
-      }
-      else {
-        //send back failed authentication error
-      }
-    }
-
-    //create a new user
-    else {
-       db.collection('users').insertOne({name: user, password: pass}, function(err, result) {
-         console.log(result);
-       });
-    }
-
-  });
-
-
-
-  //let result = db.collection('users').find( { name: user } );
-  //console.log(result);
-
-  //if they are check credentials
-    //create cookie / sign them in
-
-  //if not then sign them up / create cookie / sign them in
-
-
-});
+}); //end of post request
 
 
 
 
 io.on('connection', function(socket) {
-    //on a new connection increments the counts
-//    clientCount++;
-//    console.log("Client count: " + clientCount);
-//    users.push(`user${clientCount}`);
+
+
     socket.emit("userList", users);
-
-//
-//    socket.emit("userList", users);
-//    socket.emit("categories", rooms);
-//    socket.join("General");
-//
+    
     socket.on('connectPlayer', function(username) {
-
+        console.log("session id" + socket.id);
         console.log("User connected: " + username);
         users.push(username);
         console.log(users);
